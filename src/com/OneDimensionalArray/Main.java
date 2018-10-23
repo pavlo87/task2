05ceca3cc380a79f4e1f6e6031438d9d87b78c66
pavlo87 <zagorbenskijp@gmail.com>
2018-10-23 10:57:15 +0300

    task2

---
+++ src/com/OneDimensionalArray/Main.java
@@ -0,0 +1,24 @@
+package com.OneDimensionalArray;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array;
+        array = new int[]{12, 3, 5, 7, 13, -1, -5};
+
+        int min;
+        min = array[0];
+        int pnomer =0;
+        for (int i = 0; i < array.length; i++) {
+            if (min > Math.abs(array[i]))
+                min = (array[i]);
+            pnomer=i;
+            System.out.print(array[i]+" ");
+
+        }
+        System. out.println(" ");
+        System.out.println("Найменьше значення массиву, найблише до 0:  "+min);
+        System.out.println("Його позиційний номер: " +pnomer);
+    }
+
+}
